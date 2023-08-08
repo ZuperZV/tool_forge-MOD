296315a7c378306747ecfa86c3c1a6a84593a241
Bruger <sebastianviborgjensen@gmail.com>
2023-08-08 15:03:08 +0200

    Error

--- src/main/java/net/ZuperZV/Tool_Forge/datagen/DataGenerators.java
+++ src/main/java/net/ZuperZV/Tool_Forge/datagen/DataGenerators.java
@@ -22,8 +22,6 @@ public class DataGenerators {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
-
-        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
     }
 }
