83b9db0e277f57e1437c37965add470ef455eaad
Bruger <sebastianviborgjensen@gmail.com>
2023-09-09 13:34:09 +0200

    Error

---
+++ src/main/java/net/ZuperZV/Tool_Forge/Tool_Forge.java
@@ -0,0 +1,109 @@
+package net.ZuperZV.Tool_Forge;
+
+import com.mojang.logging.LogUtils;
+import net.ZuperZV.Tool_Forge.block.ModBlocks;
+import net.ZuperZV.Tool_Forge.block.entity.ModBlockEntities;
+import net.ZuperZV.Tool_Forge.entity.ModEntities;
+import net.ZuperZV.Tool_Forge.entity.client.SoulRenderer;
+import net.ZuperZV.Tool_Forge.item.ModItems;
+import net.ZuperZV.Tool_Forge.recipe.ModRecipes;
+import net.ZuperZV.Tool_Forge.screen.ModMenuTypes;
+import net.ZuperZV.Tool_Forge.screen.ToolStationScreen;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Tool_Forge.MOD_ID)
+public class Tool_Forge {
+    public static final String MOD_ID = "tool_forge";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Tool_Forge() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModEntities.register(modEventBus);
+
+        ModBlockEntities.register(modEventBus);
+        ModMenuTypes.register(modEventBus);
+
+        ModRecipes.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.GILDED_NETHERITE_INGOT);
+            event.accept(ModItems.GILDED_GOLD);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.GILDED_NETHERITE_SWORD);
+            event.accept(ModItems.GILDED_NETHERITE_AXE);
+
+            event.accept(ModItems.GILDED_NETHERITE_HELMET);
+            event.accept(ModItems.GILDED_NETHERITE_CHESTPLATE);
+            event.accept(ModItems.GILDED_NETHERITE_LEGGINGS);
+            event.accept(ModItems.GILDED_NETHERITE_BOOTS);
+
+
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.GILDED_NETHERITE_PICKAXE);
+            event.accept(ModItems.GILDED_NETHERITE_AXE);
+            event.accept(ModItems.GILDED_NETHERITE_SHOVEL);
+            event.accept(ModItems.GILDED_NETHERITE_HOE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
+            event.accept(ModItems.SOUL_SPAWN_EGG);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+            event.accept(ModBlocks.TOOL_STATION);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                EntityRenderers.register(ModEntities.SOUL.get(), SoulRenderer::new);
+
+                MenuScreens.register(ModMenuTypes.TOOL_STATION_MENU.get(), ToolStationScreen::new);
+            });
+        }
+    }
+}
